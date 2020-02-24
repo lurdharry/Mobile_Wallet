c52c10a46f6971b5a201c9a9c78ec1576290e04d
lurdharry <azeezhammed57@gmail.com>
2020-02-24 22:21:48 +0100

    Created Data for User details

---
+++ src/components/UserData.js
@@ -0,0 +1,17 @@
+export const User={
+    first_name:'Augusta',
+    last_name:'Fountain',
+    email:'augusta@yaypay ',
+    Account:[
+        {
+            id:1,
+            account_number:8675093658946890,
+            current_balance:585550
+        },
+        {
+            id:2,
+            account_number:6583025927590733,
+            current_balance:10123
+        },
+    ]   
+}
