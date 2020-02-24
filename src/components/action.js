2d9a896319b3716cb6f9721157c06651c43b0800
lurdharry <azeezhammed57@gmail.com>
2020-02-24 22:21:28 +0100

    Created Dummy Data for Transaction list

---
+++ src/components/action.js
@@ -0,0 +1,18 @@
+export const Actions =[
+    {
+        title:'Send Money',
+        img:'ddd'
+    },
+    {
+        title:'Recieve Money',
+        img:'ddd'
+    },
+    {
+        title:'Manage Money',
+        img:'ddd'
+    },
+    {
+        title:'Offers',
+        img:'ddd'
+    },
+]
