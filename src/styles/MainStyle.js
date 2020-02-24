bcc95fde924352efbb19b61e18abb6ad2c376d85
lurdharry <azeezhammed57@gmail.com>
2020-02-24 22:20:08 +0100

    Created Responsive Screen Components

--- src/styles/MainStyle.js
+++ src/styles/MainStyle.js
@@ -3,7 +3,7 @@ import {StyleSheet} from 'react-native'
 import {widthPercentageToDP,heightPercentageToDP,} from 'react-native-responsive-screen'
 
 export const hp =(num)=>{
-    return heightPercentageToDP((num/872)*100)
+    return heightPercentageToDP((num/812)*100)
 }
 export const wp =(num)=>{
     return widthPercentageToDP((num/375)*100)
