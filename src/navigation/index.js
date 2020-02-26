db09894b0ace057eb531ccc379a8be2b90e64378
lurdharry <azeezhammed57@gmail.com>
2020-02-26 14:21:46 +0100

    Added  RecieveMoney Screen to Main Navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,10 +1,17 @@
 import {createAppContainer} from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import Home from '../screens/Home.js'
+import SendMoney from '../screens/SendMoney'
+import RecieveMoney from '../screens/RecieveMoney'
+
 
 export default MainNavigation = createAppContainer(
     createStackNavigator({
         Home: { screen: Home,navigationOptions:{headerShown:false}},
+        SendMoney: { screen: SendMoney,navigationOptions:{headerShown:false}},
+        RecieveMoney: { screen: RecieveMoney,navigationOptions:{headerShown:false}},
+
+
 
       },
       {
