3b31900a8ddc0ca192e48cab28bad46566c547db
lurdharry <azeezhammed57@gmail.com>
2020-02-28 11:01:45 +0100

    Added  ManageMoney link to main Navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,19 +3,17 @@ import { createStackNavigator } from 'react-navigation-stack'
 import Home from '../screens/Home.js'
 import SendMoney from '../screens/SendMoney'
 import RecieveMoney from '../screens/RecieveMoney'
-
+import ManageMoney from '../screens/ManageMoney'
 
 export default MainNavigation = createAppContainer(
     createStackNavigator({
         Home: { screen: Home,navigationOptions:{headerShown:false}},
         SendMoney: { screen: SendMoney,navigationOptions:{headerShown:false}},
         RecieveMoney: { screen: RecieveMoney,navigationOptions:{headerShown:false}},
-
-
-
+        ManageMoney: { screen: ManageMoney,navigationOptions:{headerShown:false}},
       },
       {
-        initialRouteName:'Home'
+        initialRouteName:'ManageMoney'
     }
 ));
 
